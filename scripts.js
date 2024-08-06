1b72dac5c7c1b6fabeed0e13c1ad898e2e523e33
TakeJoshyy <125616429+DeathDoors@users.noreply.github.com>
2024-08-06 09:02:30 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+let selectedFilters = new Set();
+
+function filterImages(category) {
+    if (category === 'all') {
+        selectedFilters.clear();
+        selectedFilters.add('all');
+    } else {
+        if (selectedFilters.has('all')) {
+            selectedFilters.delete('all');
+        }
+        if (selectedFilters.has(category)) {
+            selectedFilters.delete(category);
+        } else {
+            selectedFilters.add(category);
+        }
+    }
+
+    console.log('Selected filters:', selectedFilters);
+    updateGallery();
+}
+
+function updateGallery() {
+    const images = document.getElementsByClassName('filter');
+
+    for (let img of images) {
+        const imgClasses = img.className.split(' ');
+        const matches = [...selectedFilters].every(filter => imgClasses.includes(filter));
+
+        if (selectedFilters.has('all') || matches) {
+            img.style.display = 'block';
+        } else {
+            img.style.display = 'none';
+        }
+    }
+}
